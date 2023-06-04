ab0a90ee52787957048a54a8be5ecc0b1bce0911
DucTuanmdt <tuanit381@gmail.com>
2023-06-04 20:24:35 +0700

    add logic get env

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
+  while (true) {
     console.log("Microservices rock!");
+    console.log("Value of env variable: ", process.env.TEST_VARIABLE);
     await sleep(5000);
   }
 }
